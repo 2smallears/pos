c376aa5a98b0e584a35ed276069dfee4161f40a4
2smallears <2smallears@users.noreply.github.com>
2016-07-18 17:15:16 +0800

    main

--- pos_v1/main/main.js
+++ pos_v1/main/main.js
@@ -1,19 +1,1 @@
 'use strict';
-
-let cartItems = getCartItems(inputs);
-let receiptItems = getReceiptItems(cartItems);
-
-function getCartItems(inputs) {
-  const allItems = loadAllItems();
-  const promotions = loadPromotions();
-
-  let cartItems = [];
-  
-  for(let i = 0; i < inputs.length; i++){
-    if (isExist(inputs, allItems)){
-      if(isDiscount(inputs, promotions)){
-      }
-    }
-  }
-}
-
